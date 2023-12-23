faf2cd371817a0f5781ed04c0ee120946ab178e7
NMBxNIGHT <thiranat_kakanmee@cmu.ac.th>
2023-12-23 17:50:24 +0700

    add solution for 3rd subproblem

--- src/cal.java
+++ src/cal.java
@@ -5,16 +5,16 @@ import java.io.IOException;
 
 public class cal {
     public static void main(String[] args) {
-        Double es = 0.00001,fx=1.0,init=1.0,fxx=1.0,x=init;
-        Integer i=0;
-        File file = new File("src/value3_2.txt");
+        Double es = 0.00001,fx=1.0,init=2.5,fxx=1.0,x=init;
+        Integer i=0,subProb=3;
+        File file = new File("src/value3_3.txt");
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(file));
             String s ;
             while(Math.abs(fx) >= es){
                 s = 'x'+i.toString()+": "+x.toString();
-                fx = Math.pow(Math.E,-x)-x;
-                fxx = -Math.pow(Math.E,-x)-1;
+                fx = cal1_3fx(x,subProb);
+                fxx = cal1_3fxx(x,subProb);
                 x = x-(fx/fxx);
                 s = s+" fx: "+fx.toString()+" fxx: "+fxx.toString();
                 writer.write(s+"\n");
@@ -25,5 +25,21 @@ public class cal {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+    public static Double cal1_3fxx(Double x,int subProb){ //1_3 mean subproblem 1-3
+        switch (subProb){
+            case 3: return 3*Math.pow(x,2);
+            case 2: return -Math.pow(Math.E,-x)-1;
+            default: return Double.MAX_VALUE;
+        }
+
+    }
+    public static Double cal1_3fx(Double x,int subProb){
+        switch (subProb){
+            case 3:return Math.pow(x,3)-48;
+            case 2:return Math.pow(Math.E,-x)-1;
+            default:return Double.MAX_VALUE;
+        }
+
     }
 }
